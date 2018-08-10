8ab3b5c6461f0c416bd57a3ca3e65ec910045818
Jingyang Zhang <jingyangzhang@10-18-189-12.dynapool.wireless.nyu.edu>
2018-08-10 11:47:33 -0400

    0808

--- app/src/main/java/com/laioffer/eventreporter/EventReportActivity.java
+++ app/src/main/java/com/laioffer/eventreporter/EventReportActivity.java
@@ -1,15 +1,26 @@
 package com.laioffer.eventreporter;
 
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventReportActivity extends AppCompatActivity {
     private static final String TAG = EventReportActivity.class.getSimpleName();
@@ -20,6 +31,10 @@ public class EventReportActivity extends AppCompatActivity {
     private ImageView mImageViewCamera;
     private DatabaseReference database;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private LocationTracker mLocationTracker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,11 +53,59 @@ public class EventReportActivity extends AppCompatActivity {
                 String key = uploadEvent();
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        //Add listener to check sign in status
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged: signed_in" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+
+        //sign in anonymously
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signInAnonymously: onComplete:" + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "signInAnonymously", task.getException());
+                }
+            }
+        });
+
+        mLocationTracker = new LocationTracker(this);
+        mLocationTracker.getLocation();
+        final double latitude = mLocationTracker.getLatitude();
+        final double longitude = mLocationTracker.getLongitude();
+
+        new AsyncTask<Void, Void, Void>() {
+            private List<String> mAddressList = new ArrayList<String>();
+
+            @Override
+            protected Void doInBackground(Void... urls) {
+                mAddressList = mLocationTracker.getCurrentLocationViaJSON(latitude,longitude);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void input) {
+                if (mAddressList.size() >= 3) {
+                    mEditTextLocation.setText(mAddressList.get(0) + ", " + mAddressList.get(1) +
+                            ", " + mAddressList.get(2) + ", " + mAddressList.get(3));
+                }
+            }
+        }.execute();
     }
 
     /**
      * Gather information inserted by user and create event for uploading. Then clear those widgets if user uploads one
-
+     *
      * @return the key of the event needs to be returned as link against Cloud storage
      */
 
@@ -80,5 +143,21 @@ public class EventReportActivity extends AppCompatActivity {
             }
         });
         return key;
+    }
+
+    @Override
+    //Add authentication listener when activity starts
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    //Remove authentication listener when activity starts
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
--- app/src/main/java/com/laioffer/eventreporter/EventsFragment.java
+++ app/src/main/java/com/laioffer/eventreporter/EventsFragment.java
@@ -37,5 +37,4 @@ public class EventsFragment extends Fragment {
         });
         return view;
     }
-
 }
